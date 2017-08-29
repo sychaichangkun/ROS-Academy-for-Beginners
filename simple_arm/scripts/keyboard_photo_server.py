b4cff54c1f56ccf74c251510f6e5b62cfc1c3cf7
robin.han <hanjf_robin@163.com>
2017-08-29 16:58:28 +0800

    just save images not show

--- simple_arm/scripts/keyboard_photo_server.py
+++ simple_arm/scripts/keyboard_photo_server.py
@@ -8,10 +8,8 @@
 import cv2
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge, CvBridgeError
-import matplotlib.pyplot as plt
 import rospy
 from simple_arm.srv import *
-
 
 COUNT=0
 
@@ -22,35 +20,16 @@ def handle_function(req):
 
     # 若request为'c', 则接收并显示图像，实现拍照功能
     if srv_command == "c":
-        print("I got c")
         # 定义图片topic，并注册到其上，接收到消息后调用callback_topic
         img_topic = "/rgb_camera/image_raw"
-        print "image topic OK"
-        #img_sub = rospy.Subscriber(img_topic, Image, callback_topic)
 	img_msg = rospy.wait_for_message(img_topic, Image)
-
-        print type(img_msg)
-
         try:
             global COUNT 
             COUNT = COUNT+1
             bridge = CvBridge()
-            print "Hello 0"
             cv_image = bridge.imgmsg_to_cv2(img_msg,'bgr8')
-            print "Hello 1", cv_image.shape, cv_image.dtype
-            cv2.imwrite("image%s.png",cv_image)%COUNT
-            cv2.imshow("Image", cv_image)
-            print "Hello 2"
+            cv2.imwrite("images/image%s.png"%COUNT,cv_image)
             cv2.waitKey(0)
-            print "Hello 3"
-	    cv2.destroyWindow("Image")
-            cv2.waitKey(1)
-            cv2.waitKey(1)
-            cv2.waitKey(1)
-            cv2.waitKey(1)
-            cv2.waitKey(1)
-            cv2.waitKey(1)
-            print "Hello 4"
 
         except CvBridgeError as e:
             print(e)
@@ -61,11 +40,8 @@ def handle_function(req):
 
 def take_photo_server():
     rospy.init_node("take_photo_server")
-    print "Hello 10"
     s = rospy.Service('take_photo', TakePhotoCommand, handle_function)
-    print "Hello 11"
     rospy.spin()
-    print "Hello 12"
 
 if __name__ == "__main__":
     take_photo_server()
