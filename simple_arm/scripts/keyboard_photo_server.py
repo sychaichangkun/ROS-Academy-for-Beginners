c416e6216e65c16ea346f2a51808cad75ff0921a
robin.han <hanjf_robin@163.com>
2017-08-31 12:41:26 +0800

    complete server file

--- simple_arm/scripts/keyboard_photo_server.py
+++ simple_arm/scripts/keyboard_photo_server.py
@@ -5,13 +5,12 @@
 # 'c'，如果是，则从/rgb_camera/image_raw这个相机图片topic中接收一张图片，然后显示
 # 出来
 import os
+from datetime import datetime
 import cv2
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge, CvBridgeError
 import rospy
 from simple_arm.srv import *
-
-COUNT=0
 
 def handle_function(req):
 
@@ -24,12 +23,10 @@ def handle_function(req):
         img_topic = "/rgb_camera/image_raw"
 	img_msg = rospy.wait_for_message(img_topic, Image)
         try:
-            BASE_DIR = os.path.dirname()
-            global COUNT 
-            COUNT = COUNT+1
             bridge = CvBridge()
             cv_image = bridge.imgmsg_to_cv2(img_msg,'bgr8')
-            cv2.imwrite("image%s.png"%COUNT,cv_image)
+            image_name = datetime.now().strftime('%Y-%m-%d_%H:%M:%S')+".png"
+            cv2.imwrite(image_name,cv_image)
             cv2.waitKey(0)
 
         except CvBridgeError as e:
