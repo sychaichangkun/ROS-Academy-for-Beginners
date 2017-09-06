468a797b022588cd57ecb6d4ef0c280df2b97271
robin.han <hanjf_robin@163.com>
2017-09-06 23:33:29 +0800

    add comments to service client and server

--- service_client_demo/scripts/client_demo.py
+++ service_client_demo/scripts/client_demo.py
@@ -1,17 +1,24 @@
 #!/usr/bin/env python
 # coding:utf-8
 
+# Import the modules that we need
 import rospy
 from service_client_demo.srv import *
 
 def client_srv():
+
+    # Wait the service named "greetings" to be available
     rospy.wait_for_service("greetings")
     try:
+        # Define the service client with service name "greetings" and service type Service_demo
         greetings_client = rospy.ServiceProxy("greetings",Service_demo)
+
+        # Request values name "HAN" and age 20 
         resp = greetings_client("HAN",20)
         print "Message From server:%s"%resp.response
     except rospy.ServiceException, e:
         print "Service call failed: %s"%e
 
+# if run this file alone, call the function client_srv()
 if __name__=="__main__":
     client_srv()
--- service_client_demo/scripts/server_demo.py
+++ service_client_demo/scripts/server_demo.py
@@ -1,17 +1,31 @@
 #!/usr/bin/env python
 # coding:utf-8
 
+# Import necessary modules. Attention that how we import the service files.
+# the srv is a folder under the service_client_demo package. 
 import rospy
 from service_client_demo.srv import *
 
 def server_srv():
+    # Init the node with name "greetings_server"
     rospy.init_node("greetings_server")
+    # Define the service server with name "greetings" and service type Service_demo
+    # and request message will be handled by handle_function.
     s = rospy.Service("greetings", Service_demo, handle_function)
     print "Ready to handle the request:"
+    # avoid the server from quiting until the node shutdown
     rospy.spin()
 
+# Define the handle function to handle the request inputs
 def handle_function(req):
+    # Please look out how we use the request information, the service is like a class 
+    # If we want to use the information in it, for example, in our example, the Service_demo.srv
+    # file contains two parts, the first one is request part, and the second one is response part
+    # if we want to use the request information(name with type string and age with type int64), 
+    # we can call them as req.name and req.age
     print "Hi Server, my name is %s and I'm %s years old"%(req.name,req.age)
+
+    # The Service_demoResponse class is instanced automatically by ROS once the service is defined.`
     return Service_demoResponse("Hi %s"%req.name)
 
 if __name__=="__main__":
