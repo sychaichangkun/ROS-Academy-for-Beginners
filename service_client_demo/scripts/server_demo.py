dd33205d1065c06c1c14b54ce358179d7fd365bf
robin.han <hanjf_robin@163.com>
2017-09-06 08:50:39 +0800

    complete service_client_demo

--- service_client_demo/scripts/server_demo.py
+++ service_client_demo/scripts/server_demo.py
@@ -2,17 +2,17 @@
 # coding:utf-8
 
 import rospy
-from service_client_demo.srv import SERVICE_DEMO
+from service_client_demo.srv import *
 
 def server_srv():
     rospy.init_node("greetings_server")
-    s = rospy.Service("greetings", SERVICE_DEMO, handle_function)
+    s = rospy.Service("greetings", Service_demo, handle_function)
     print "Ready to handle the request:"
     rospy.spin()
 
 def handle_function(req):
     print "Hi Server, my name is %s and I'm %s years old"%(req.name,req.age)
-    return SERVICE_DEMOResponse("Hi %s"%req.name)
+    return Service_demoResponse("Hi %s"%req.name)
 
 if __name__=="__main__":
     server_srv()
