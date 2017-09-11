5e46984db5dadf3d5693037aa0d5ed0d1def4174
robin.han <hanjf_robin@163.com>
2017-09-11 08:56:56 +0800

    添加中文注释

--- service_client_demo/scripts/server_demo.py
+++ service_client_demo/scripts/server_demo.py
@@ -1,32 +1,34 @@
 #!/usr/bin/env python
 # coding:utf-8
 
-# Import necessary modules. Attention that how we import the service files.
-# the srv is a folder under the service_client_demo package. 
+# 上面指定编码utf-8，使python能够识别中文
+
+# 加载必需模块，注意service模块的加载方式，from 包名.srv import *
+# 其中srv指的是在包根目录下的srv文件夹，也即srv模块
 import rospy
 from service_client_demo.srv import *
 
 def server_srv():
-    # Init the node with name "greetings_server"
+    # 初始化节点，命名为 "greetings_server"
     rospy.init_node("greetings_server")
-    # Define the service server with name "greetings" and service type Service_demo
-    # and request message will be handled by handle_function.
+    # 定义service的server端，service名称为"greetings"， service类型为Service_demo
+    # 收到的request请求信息将作为参数传递给handle_function进行处理
     s = rospy.Service("greetings", Service_demo, handle_function)
     print "Ready to handle the request:"
-    # avoid the server from quiting until the node shutdown
+    # 阻止程序结束
     rospy.spin()
 
 # Define the handle function to handle the request inputs
 def handle_function(req):
-    # Please look out how we use the request information, the service is like a class 
-    # If we want to use the information in it, for example, in our example, the Service_demo.srv
-    # file contains two parts, the first one is request part, and the second one is response part
-    # if we want to use the request information(name with type string and age with type int64), 
-    # we can call them as req.name and req.age
+    
+    # 注意我们是如何调用request请求内容的，与前面client端相似，都是将其认为是一个对象的属性，通过对象调用属性，在我们定义
+    # 的Service_demo类型的service中，request部分的内容包含两个变量，一个是字符串类型的name，另外一个是整数类型的age
     print "Hi Server, my name is %s and I'm %s years old"%(req.name,req.age)
 
-    # The Service_demoResponse class is instanced automatically by ROS once the service is defined.`
+    # 返回一个Service_demoResponse实例化对象，其实就是返回一个response的对象，其包含的内容为我们再Service_demo.srv中定义的
+    # response部分的内容，我们定义了一个string类型的变量，因此，此处实例化时传入字符串即可
     return Service_demoResponse("Hi %s"%req.name)
 
+# 如果单独运行此文件，则将上面定义的server_srv作为主函数运行
 if __name__=="__main__":
     server_srv()
