19c2c72051ca2b23068a167f421f33e81ce33802
“davidhan” <619409713@qq.com>
2017-09-23 22:37:28 +0800

    使用ROS的数据结构

--- topic_demo/src/talker.cpp
+++ topic_demo/src/talker.cpp
@@ -1,10 +1,10 @@
 // ROS 包含的目录.
 #include <ros/ros.h>
 #include <string>
-
 // 包含ROS的msg机制产生的头文件
 #include <topic_demo/gps.h>
-
+#include <std_msgs/Float32.h>
+//ROS当中
 int main(int argc, char **argv)
 {
   // 初始化
@@ -13,32 +13,26 @@ int main(int argc, char **argv)
   ros::NodeHandle nh;
   //实例化自定义msg
    topic_demo::gps msg;
-
   //! 发布消息
   ros::Publisher pub_;
-
   //! msg 的x值.
-  float x=1.0;
-
-
+  std_msgs::Float32 x;
   //! msg 的y值
-  float y=2.0;
- 
-  //! msg 的状态
+  std_msgs::Float32 y;
   std::string state("working");
-
+  x.data=1.0000;
+  y.data=1.0000;
+  //! msg 的状态
   pub_ = nh.advertise<topic_demo::gps>("chatter", 10);
   //定义发布的频率 
   ros::Rate loop_rate(1);
-
-
- while (ros::ok())
+  while (ros::ok())
   {
-    msg.x = x;
-    msg.y = y;
+    msg.x = x.data;
+    msg.y = y.data;
   //线性比例变换，每隔1秒更新一次
-    x=1.03*x;
-    y=1.01*y;
+    x.data=1.03*x.data;
+    y.data=1.01*y.data;
     msg.state=state.c_str();
     ROS_INFO("Talker: GPS: x = %f, y = %f ",  msg.x ,msg.y);
   //以1Hz的频率发布msg
@@ -46,7 +40,6 @@ int main(int argc, char **argv)
     ros::spinOnce();//不是必需的，但是保持增加这个调用，是好习惯。
     loop_rate.sleep();//根据前面的定义的loop_rate,设置1s的暂停
   }
-
   return 0;
 } 
 
