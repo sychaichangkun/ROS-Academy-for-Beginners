004944743df6f6effc38a67252b6d4464d7d3032
robin.han <hanjf_robin@163.com>
2017-09-26 09:42:41 +0800

    add parameter demo version C++

---
+++ param_demo/src/param_demo.cpp
@@ -0,0 +1,21 @@
+#include<ros/ros.h>
+
+int main(int argc, char **argv){
+	ros::init(argc, argv, "param_demo");
+	ros::NodeHandle nh;
+	int init_parameter = 20;
+	ros::param::set("/Param_demo",init_parameter);
+	ros::Rate rate(1);
+
+	while(ros::ok()){
+		int parameter = 0;
+		ros::param::get("/Param_demo", parameter);
+		if(parameter!=init_parameter){
+			ROS_INFO("You've changed parameter /Param_demo from %d to %d", init_parameter, parameter);
+			init_parameter = parameter;
+		}
+		ROS_INFO("parameter /Param_demo = %d", parameter);
+
+		rate.sleep();
+	}
+}
