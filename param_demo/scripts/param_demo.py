e5ff00cdf2e0ac814abb860be8a548a700e332f9
Changkun <sychaichangkun@gmail.com>
2017-11-24 17:13:06 +0800

    modify param_demo python

--- param_demo/scripts/param_demo.py
+++ param_demo/scripts/param_demo.py
@@ -5,17 +5,32 @@ import rospy
 
 def param_demo():
     rospy.init_node("param_demo")
+    rate = rospy.Rate(1)
     while(not rospy.is_shutdown()):
-        parameter = rospy.get_param("/Param_demo")
-        if not(parameter==init_parameter):
-            print "You've changed parameter '/Param_demo' from %s to %s"%(init_parameter, parameter)
-            global init_parameter
-            init_parameter = parameter
+        #get param
+        parameter1 = rospy.get_param("/param1")
+        parameter2 = rospy.get_param("/param2", default=222)
+        rospy.loginfo('Get param1 = %d', parameter1)
+        rospy.loginfo('Get param2 = %d', parameter2)
 
+        #delete param
+        rospy.delete_param('/param2')
 
-        print "Param_demo=%s"%parameter
-        rospy.sleep(1)
+        #set param
+        rospy.set_param('/param2',2)
+        
+        #check param
+        ifparam3 = rospy.has_param('/param3')
+        if(ifparam3):
+            rospy.loginfo('/param3 exists')
+        else:
+            rospy.loginfo('/param3 does not exist')
+
+        #get all param names
+        params = rospy.get_param_names()
+        rospy.loginfo('param list: %s', params)
+
+        rate.sleep()
 
 if __name__=="__main__":
-    init_parameter = rospy.get_param("/Param_demo")
     param_demo()
